d280460e64d89667a8995746ac86df2aee861af3
Ichihoshisuz <81299558+Ichihoshisuz@users.noreply.github.com>
2021-03-24 19:20:21 +0800

    Chapter 6

---
+++ Ch 06/6.3.cpp
@@ -0,0 +1,19 @@
+/*
+#include <iostream>
+using namespace std;
+int fact(int a, int b)
+{
+	++b;
+	a *= b;
+	return a;
+}
+int main()
+{
+	int a, b, c;
+	cin >> a >> b;
+	c = fact(a, b);
+	cout << c << endl;
+	system("pause");
+	return 0;
+}
+*/
